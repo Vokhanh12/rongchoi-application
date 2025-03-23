571fe943692b026c7c08c1c6049fdb0748b39a3c
Vokhanh12 <khanhyou2018@gmail.com>
2025-03-23 11:10:40 +0700

    add commit

--- moblie-rongchoi-app/lib/features/presentation/widgets/custome_column_data.dart
+++ moblie-rongchoi-app/lib/features/presentation/widgets/custome_column_data.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 
+
 class CustomeColumnData extends StatelessWidget {
+
 
   const CustomeColumnData({super.key, required this.children});
 
   final List<Widget> children;
 
+  
+
   @override
   Widget build(BuildContext context) {
+
+
     return Column(
       children: children
     );
--- moblie-rongchoi-app/lib/main.dart
+++ moblie-rongchoi-app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rongchoi_application/core/constants/corlos.dart';
 import 'package:rongchoi_application/core/constants/strings.dart';
@@ -9,7 +10,7 @@ import 'package:rongchoi_application/features/domain/entities/tranlations_entity
 import 'package:rongchoi_application/features/presentation/bloc/tranlation_bloc/tranlation_bloc.dart';
 import 'package:rongchoi_application/injection_container.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = RCBlocObserver();
 
@@ -18,23 +19,50 @@ void main() async{
   final dbHelper = DatabaseHelper();
   final store = await dbHelper.store;
 
-  final tranlations_1 = TranlationsEntity(id: 1, code: 'RC.Username', tranlationVi: 'Tên đăng nhập', tranlationEn: 'Username');
-  final tranlations_2 = TranlationsEntity(id: 2, code: 'RC.Password', tranlationVi: 'Mật khẩu', tranlationEn: 'Password');
-  final tranlations_3 = TranlationsEntity(id: 3, code: 'RC.ForgotPassword', tranlationVi: 'Quên mật khẩu', tranlationEn: 'Forgot password?');
-  final tranlations_4 = TranlationsEntity(id: 4, code: 'RC.Login', tranlationVi: 'Đăng nhập', tranlationEn: 'Login');
+  final tranlations_1 = TranlationsEntity(
+      id: 1,
+      code: 'RC.Username',
+      tranlationVi: 'Tên đăng nhập',
+      tranlationEn: 'Username');
+  final tranlations_2 = TranlationsEntity(
+      id: 2,
+      code: 'RC.Password',
+      tranlationVi: 'Mật khẩu',
+      tranlationEn: 'Password');
+  final tranlations_3 = TranlationsEntity(
+      id: 3,
+      code: 'RC.ForgotPassword',
+      tranlationVi: 'Quên mật khẩu',
+      tranlationEn: 'Forgot password?');
+  final tranlations_4 = TranlationsEntity(
+      id: 4,
+      code: 'RC.Login',
+      tranlationVi: 'Đăng nhập',
+      tranlationEn: 'Login');
+  final tranlations_5 = TranlationsEntity(
+      id: 5,
+      code: 'RC.HaveAccount',
+      tranlationVi: 'Chưa có tài khoản',
+      tranlationEn: "Don't have an account?");
+  final tranlations_6 = TranlationsEntity(
+      id: 6,
+      code: 'RC.SignUpNow',
+      tranlationVi: 'Đăng ký ngay',
+      tranlationEn: "Sign up now");
   await dbHelper.saveTranlationLocal(tranlations_1);
   await dbHelper.saveTranlationLocal(tranlations_2);
   await dbHelper.saveTranlationLocal(tranlations_3);
   await dbHelper.saveTranlationLocal(tranlations_4);
+  await dbHelper.saveTranlationLocal(tranlations_5);
+  await dbHelper.saveTranlationLocal(tranlations_6);
 
-  print("test"); 
+  print("test");
 
   var test = await dbHelper.getAllTranslationsLocal();
   print(test);
 
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -46,16 +74,19 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   void initState() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: [SystemUiOverlay.bottom, SystemUiOverlay.top]);
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
-      providers: [
-          BlocProvider<TranlationBloc>(create: (context) => locator<TranlationBloc>()),
-      ],
-        child:  MaterialApp(
+        providers: [
+          BlocProvider<TranlationBloc>(
+              create: (context) => locator<TranlationBloc>()),
+        ],
+        child: MaterialApp(
           title: 'Nibbles',
           debugShowCheckedModeBanner: false,
           onGenerateRoute: AppRouter.onGenerateRoute,
@@ -64,7 +95,6 @@ class _MyAppState extends State<MyApp> {
             fontFamily: AppStrings.fontFamily,
             scaffoldBackgroundColor: AppColors.MA_SFBACKGROUP_COLOR,
           ),
-        )
-    );
+        ));
   }
 }
